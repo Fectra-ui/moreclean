43dd6c96e259dbd417066103b5c952624d46694b
Bermudatanker-spec <244728374+bermudatanker-spec@users.noreply.github.com>
2026-04-25 08:04:51 +0200

    Add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -87,6 +88,20 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         {children}
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-K5064BTGGY"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-K5064BTGGY');
+          `}
+        </Script>
 
         <script
           type="application/ld+json"
